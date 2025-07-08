0ff324fa9c6f9da2122883ec2f97253c03d5085f
Clownacy <Clownacy@users.noreply.github.com>
2025-07-08 22:47:53 +0100

    Update common Lua code.
    Smaller build script; yay!

--- build.lua
+++ build.lua
@@ -13,20 +13,18 @@ local improved_dac_driver_compression = false
 -- End of settings --
 ---------------------
 
+-------------------------------------
+-- Actual build script begins here --
+-------------------------------------
+
 local common = require "build_tools.lua.common"
 
+-- Build the ROM.
 local compression = improved_dac_driver_compression and "kosinski-optimised" or "kosinski"
-local message, abort = common.build_rom("sonic", "s1built", "", "-p=FF -z=0," .. compression .. ",Size_of_DAC_driver_guess,after", false, "https://github.com/sonicretro/s1disasm")
-
-if message then
-	exit_code = false
-end
-
-if abort then
-	os.exit(exit_code, true)
-end
+common.build_rom_and_handle_failure("sonic", "s1built", "", "-p=FF -z=0," .. compression .. ",Size_of_DAC_driver_guess,after", false, "https://github.com/sonicretro/s1disasm")
 
 -- Correct the ROM's header with a proper checksum and end-of-ROM value.
 common.fix_header("s1built.bin")
 
-os.exit(exit_code, false)
+-- A successful build; we can quit now.
+common.exit()
