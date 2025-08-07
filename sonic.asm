08b3a68f050a847ba7b4e028d37d5517bd340242
Clownacy <Clownacy@users.noreply.github.com>
2025-08-07 17:10:56 +0100

    Correct a typo in a comment.
    V E S I O N

--- sonic.asm
+++ sonic.asm
@@ -14,7 +14,7 @@ BackupSRAM	  = 1
 AddressSRAM	  = 3	; 0 = odd+even; 2 = even only; 3 = odd only
 
 ; Change to 0 to build the original version of the game, dubbed REV00
-; Change to 1 to build the later vesion, dubbed REV01, which includes various bugfixes and enhancements
+; Change to 1 to build the later version, dubbed REV01, which includes various bugfixes and enhancements
 ; Change to 2 to build the version from Sonic Mega Collection, dubbed REVXB, which fixes the infamous "spike bug"
 Revision	  = 1
 
