25a61b0f4dcaa4e5223ae2ba3ff2910fc0e329fc
Clownacy <Clownacy@users.noreply.github.com>
2025-08-30 15:31:13 +0100

    Tag the bugfix in the Scrap Brain music with '[BUG]'.
    As discussed in https://github.com/sonicretro/s2disasm/issues/76#issuecomment-3193193691

--- sound/music/Mus86 - SBZ.asm
+++ sound/music/Mus86 - SBZ.asm
@@ -125,7 +125,7 @@ Mus86_SBZ_Loop06:
 	smpsCall            Mus86_SBZ_Call07
 	smpsLoop            $00, $02, Mus86_SBZ_Loop06
 	smpsAlterVol        $08
-	; The detune in this channel is not reset upon loop,
+	; [BUG] The detune in this channel is not reset upon loop,
 	;   thus causing the looped intro to be slightly off-tune.
 	;   Uncomment the following line to fix this.
 	;smpsAlterNote       $00
