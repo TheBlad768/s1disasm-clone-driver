f4f60d3ff0c2ba5e225db15fc8b57cd0b3f41a79
Mildanner <cianocriativo408@gmail.com>
2025-11-06 14:15:30 -0300

    Replace the RAM of Z80 Port to Variable
    Found a Hivebrain leftover, now it's adjusted.

--- sonic.asm
+++ sonic.asm
@@ -912,9 +912,9 @@ JoypadInit:
 		stopZ80
 		waitZ80
 		moveq	#$40,d0
-		move.b	d0,($A10009).l	; init port 1 (joypad 1)
-		move.b	d0,($A1000B).l	; init port 2 (joypad 2)
-		move.b	d0,($A1000D).l	; init port 3 (expansion/extra)
+		move.b	d0,(z80_port_1_control+1).l	; init port 1 (joypad 1)
+		move.b	d0,(z80_port_2_control+1).l	; init port 2 (joypad 2)
+		move.b	d0,(z80_expansion_control+1).l	; init port 3 (expansion/extra)
 		startZ80
 		rts	
 ; End of function JoypadInit
