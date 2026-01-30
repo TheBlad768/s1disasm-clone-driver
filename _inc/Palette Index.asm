e2c269652a9bc83a445c03e7fb604ee0cabc8f8d
vladikcomper <rubstov@gmail.com>
2026-01-30 21:49:37 +0100

    Further simplify and improve `_inc/Palette Index.asm`
    Previous implementation had an issue, where it was impossible to create distinct palette entries pointing to the same palette data, e.g. having `paletteIndexEntry Pal_Sonic, v_palette_line_1` and `paletteIndexEntry Pal_Sonic, v_palette_line_2` in the same table would produce a compile error.

--- _inc/Palette Index.asm
+++ _inc/Palette Index.asm
@@ -2,68 +2,40 @@
 ; Palette index
 ; ---------------------------------------------------------------------------
 
-paletteIndexEntry:	macro paletteLabel,paletteRAMaddress,{GLOBALSYMBOLS}
-
-ptr_paletteLabel:
+makePalEntry:	macro paletteLabel,paletteRAMaddress,{INTLABEL},{GLOBALSYMBOLS}
+__LABEL__: = (*-Pal_Index)/8
 	dc.l paletteLabel
 	dc.w paletteRAMaddress,(paletteLabel_end-paletteLabel)/4-1
 	endm
 ; ---------------------------------------------------------------------------
 
 Pal_Index:
-	; FORMAT:		Palette label,		RAM location
-	; Palette size is calculated dynamically using an end marker made by bincludeEndMarker
-	paletteIndexEntry	Pal_SegaBG, 		v_palette_line_1
-	paletteIndexEntry	Pal_Title,		v_palette_line_1
-	paletteIndexEntry	Pal_LevelSel,		v_palette_line_1
-	paletteIndexEntry	Pal_Sonic,		v_palette_line_1
-	
+
+; Id			Palette label,		RAM location
+; NOTE: Palette size is calculated dynamically using an end marker made by bincludeEndMarker
+palid_SegaBG:		makePalEntry	Pal_SegaBG, 		v_palette_line_1
+palid_Title:		makePalEntry	Pal_Title,		v_palette_line_1
+palid_LevelSel:		makePalEntry	Pal_LevelSel,		v_palette_line_1
+palid_Sonic:		makePalEntry	Pal_Sonic,		v_palette_line_1
+
 Pal_Levels:
-	paletteIndexEntry	Pal_GHZ, 		v_palette_line_2
-	paletteIndexEntry	Pal_LZ, 		v_palette_line_2
-	paletteIndexEntry	Pal_MZ, 		v_palette_line_2
-	paletteIndexEntry	Pal_SLZ,		v_palette_line_2
-	paletteIndexEntry	Pal_SYZ,		v_palette_line_2
-	paletteIndexEntry	Pal_SBZ1, 		v_palette_line_2
+
+palid_GHZ:		makePalEntry	Pal_GHZ, 		v_palette_line_2
+palid_LZ:		makePalEntry	Pal_LZ, 		v_palette_line_2
+palid_MZ:		makePalEntry	Pal_MZ, 		v_palette_line_2
+palid_SLZ:		makePalEntry	Pal_SLZ,		v_palette_line_2
+palid_SYZ:		makePalEntry	Pal_SYZ,		v_palette_line_2
+palid_SBZ1:		makePalEntry	Pal_SBZ1, 		v_palette_line_2
 	zonewarning Pal_Levels,8
 
-	paletteIndexEntry	Pal_Special, 		v_palette_line_1
-	paletteIndexEntry	Pal_LZWater, 		v_palette_line_1
-	paletteIndexEntry	Pal_SBZ3, 		v_palette_line_2
-	paletteIndexEntry	Pal_SBZ3Water, 		v_palette_line_1
-	paletteIndexEntry	Pal_SBZ2, 		v_palette_line_2
-	paletteIndexEntry	Pal_LZSonWater,		v_palette_line_1
-	paletteIndexEntry	Pal_SBZ3SonWat,		v_palette_line_1
-	paletteIndexEntry	Pal_SSResult, 		v_palette_line_1
-	paletteIndexEntry	Pal_Continue, 		v_palette_line_1
-	paletteIndexEntry	Pal_Ending, 		v_palette_line_1
+palid_Special:		makePalEntry	Pal_Special, 		v_palette_line_1
+palid_LZWater:		makePalEntry	Pal_LZWater, 		v_palette_line_1
+palid_SBZ3:		makePalEntry	Pal_SBZ3, 		v_palette_line_2
+palid_SBZ3Water:	makePalEntry	Pal_SBZ3Water, 		v_palette_line_1
+palid_SBZ2:		makePalEntry	Pal_SBZ2, 		v_palette_line_2
+palid_LZSonWater:	makePalEntry	Pal_LZSonWater,		v_palette_line_1
+palid_SBZ3SonWat:	makePalEntry	Pal_SBZ3SonWat,		v_palette_line_1
+palid_SSResult:		makePalEntry	Pal_SSResult, 		v_palette_line_1
+palid_Continue:		makePalEntry	Pal_Continue, 		v_palette_line_1
+palid_Ending:		makePalEntry	Pal_Ending, 		v_palette_line_1
 	even
-
-; ---------------------------------------------------------------------------
-; Palette index IDs
-; ---------------------------------------------------------------------------
-
-offset :=	Pal_Index
-ptrsize :=	8
-idstart :=	0
-
-palid_SegaBG:		equ id(ptr_Pal_SegaBG)
-palid_Title:		equ id(ptr_Pal_Title)
-palid_LevelSel:		equ id(ptr_Pal_LevelSel)
-palid_Sonic:		equ id(ptr_Pal_Sonic)
-palid_GHZ:		equ id(ptr_Pal_GHZ)
-palid_LZ:		equ id(ptr_Pal_LZ)
-palid_MZ:		equ id(ptr_Pal_MZ)
-palid_SLZ:		equ id(ptr_Pal_SLZ)
-palid_SYZ:		equ id(ptr_Pal_SYZ)
-palid_SBZ1:		equ id(ptr_Pal_SBZ1)
-palid_Special:		equ id(ptr_Pal_Special)
-palid_LZWater:		equ id(ptr_Pal_LZWater)
-palid_SBZ3:		equ id(ptr_Pal_SBZ3)
-palid_SBZ3Water:	equ id(ptr_Pal_SBZ3Water)
-palid_SBZ2:		equ id(ptr_Pal_SBZ2)
-palid_LZSonWater:	equ id(ptr_Pal_LZSonWater)
-palid_SBZ3SonWat:	equ id(ptr_Pal_SBZ3SonWat)
-palid_SSResult:		equ id(ptr_Pal_SSResult)
-palid_Continue:		equ id(ptr_Pal_Continue)
-palid_Ending:		equ id(ptr_Pal_Ending)
