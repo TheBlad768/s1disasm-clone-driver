4c5469566be0da6ac7f1d8b7f6ce3ffb497b621d
Selbi182 <shadow93x@gmail.com>
2026-01-30 16:56:24 +0100

    Print build.bat output to console
    This does not affect the sink to errors.txt, it simply prints that file to the console after the build has completed.

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@
 
 IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
 asm68k /k /m /p /o ae-,c+,l+ sonic.asm, s1built.bin >errors.txt, , sonic.lst
+type errors.txt
 fixheadr.exe s1built.bin
