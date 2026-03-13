1bf51410f34e45fc42de7bc375bb9a0816d5bdbf
Selbi182 <shadow93x@gmail.com>
2026-03-13 23:31:09 +0100

    Moved error.txt to sonic.log
    This matches the file name used in the AS branch.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
-asm68k /k /m /p /o ae-,oz+,c+,l+ sonic.asm, s1built.bin >errors.txt, , sonic.lst
-type errors.txt
+asm68k /k /m /p /o ae-,oz+,c+,l+ sonic.asm, s1built.bin >sonic.log, , sonic.lst
+type sonic.log
 fixheadr.exe s1built.bin
