919aef81d70f11610a118cfd622bf10394d836e7
Selbi182 <shadow93x@gmail.com>
2026-04-02 00:30:44 +0200

    Updated ASM68K build.bat to pause on failure
    This mimics the behavior of the AS branch build.bat

--- build.bat
+++ build.bat
@@ -2,5 +2,10 @@
 
 IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
 "build_tools\asm68k.exe" /k /m /p /o ae-,oz+,c+,l+ sonic.asm, s1built.bin >sonic.log, , sonic.lst
-type sonic.log
-"build_tools\fixheadr.exe" s1built.bin
+if exist s1built.bin (
+    "build_tools\fixheadr.exe" s1built.bin	
+) else (
+    type sonic.log
+    echo.
+    pause REM // Pause on failure so that the user can read the error message.
+)	
