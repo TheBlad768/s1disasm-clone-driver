a80cc4538fa9ebb8a4397ab2da235f6c14fbe792
Selbi182 <shadow93x@gmail.com>
2026-04-03 01:10:16 +0200

    Introduced lvlsize macro to make LevelSizeArray slightly less verbose
    Also added an extra note to LevelOrder.asm

--- _inc/LevelSizeArray.asm
+++ _inc/LevelSizeArray.asm
@@ -2,45 +2,52 @@
 ; Level size array
 ; ---------------------------------------------------------------------------
 
-		;    |----------------------------------------Unused
-		;    |      |---------------------------------Left boundary
-		;    |      |      |--------------------------Right boundary
-		;    |      |      |      |-------------------Top boundary
-		;    |      |      |      |      |------------Bottom boundary
-		; GHZ|      |      |      |      |      |-----Vertical screen shift (redundant)
-		dc.w $0004, $0000, $24BF, $0000, $0300, $0060
-		dc.w $0004, $0000, $1EBF, $0000, $0300, $0060
-		dc.w $0004, $0000, $2960, $0000, $0300, $0060
-		dc.w $0004, $0000, $2ABF, $0000, $0300, $0060
-		; LZ
-		dc.w $0004, $0000, $19BF, $0000, $0530, $0060
-		dc.w $0004, $0000, $10AF, $0000, $0720, $0060
-		dc.w $0004, $0000, $202F, $FF00, $0800, $0060
-		dc.w $0004, $0000, $20BF, $0000, $0720, $0060
-		; MZ
-		dc.w $0004, $0000, $17BF, $0000, $01D0, $0060
-		dc.w $0004, $0000, $17BF, $0000, $0520, $0060
-		dc.w $0004, $0000, $1800, $0000, $0720, $0060
-		dc.w $0004, $0000, $16BF, $0000, $0720, $0060
-		; SLZ
-		dc.w $0004, $0000, $1FBF, $0000, $0640, $0060
-		dc.w $0004, $0000, $1FBF, $0000, $0640, $0060
-		dc.w $0004, $0000, $2000, $0000, $06C0, $0060
-		dc.w $0004, $0000, $3EC0, $0000, $0720, $0060
-		; SYZ
-		dc.w $0004, $0000, $22C0, $0000, $0420, $0060
-		dc.w $0004, $0000, $28C0, $0000, $0520, $0060
-		dc.w $0004, $0000, $2C00, $0000, $0620, $0060
-		dc.w $0004, $0000, $2EC0, $0000, $0620, $0060
-		; SBZ
-		dc.w $0004, $0000, $21C0, $0000, $0720, $0060
-		dc.w $0004, $0000, $1E40, $FF00, $0800, $0060
-		dc.w $0004, $2080, $2460, $0510, $0510, $0060
-		dc.w $0004, $0000, $3EC0, $0000, $0720, $0060
+lvlsize macro left,right,top,bottom
+	; $0004 is an unused value, $0060 is the default vertical screen shift.
+	; Both are always the same and redundant.
+	dc.w $0004, left, right, top, bottom, $0060
+	endm
+
+; ---------------------------------------------------------------------------
+
+		;           |---------------------------------Left boundary
+		;           |      |--------------------------Right boundary
+		;           |      |      |-------------------Top boundary
+		;           |      |      |      |------------Bottom boundary
+		; GHZ       |      |      |      |
+		lvlsize     0, $24BF,     0,  $300 ; GHZ1
+		lvlsize     0, $1EBF,     0,  $300 ; GHZ2
+		lvlsize     0, $2960,     0,  $300 ; GHZ3
+		lvlsize     0, $2ABF,     0,  $300 ; GHZ4 (unused)
+		; LZ                          
+		lvlsize     0, $19BF,     0,  $530 ; LZ1
+		lvlsize     0, $10AF,     0,  $720 ; LZ2
+		lvlsize     0, $202F, -$100,  $800 ; LZ3 (level wrapping)
+		lvlsize     0, $20BF,     0,  $720 ; LZ4 (SBZ3)
+		; MZ                          
+		lvlsize     0, $17BF,     0,  $1D0 ; MZ1
+		lvlsize     0, $17BF,     0,  $520 ; MZ2
+		lvlsize     0, $1800,     0,  $720 ; MZ3
+		lvlsize     0, $16BF,     0,  $720 ; MZ4 (unused)
+		; SLZ                         
+		lvlsize     0, $1FBF,     0,  $640 ; SLZ1
+		lvlsize     0, $1FBF,     0,  $640 ; SLZ2
+		lvlsize     0, $2000,     0,  $6C0 ; SLZ3
+		lvlsize     0, $3EC0,     0,  $720 ; SLZ4 (unused)
+		; SYZ                         
+		lvlsize     0, $22C0,     0,  $420 ; SYZ1
+		lvlsize     0, $28C0,     0,  $520 ; SYZ2
+		lvlsize     0, $2C00,     0,  $620 ; SYZ3
+		lvlsize     0, $2EC0,     0,  $620 ; SYZ4 (unused)
+		; SBZ                         
+		lvlsize     0, $21C0,     0,  $720 ; SBZ1
+		lvlsize     0, $1E40, -$100,  $800 ; SBZ2 (level wrapping)
+		lvlsize $2080, $2460,  $510,  $510 ; SBZ3 (FZ)
+		lvlsize     0, $3EC0,     0,  $720 ; SBZ4 (unused)
 		zonewarning LevelSizeArray,$30
 		; Ending
-		dc.w $0004, $0000, $0500, $0110, $0110, $0060
-		dc.w $0004, $0000, $0DC0, $0110, $0110, $0060
-		dc.w $0004, $0000, $2FFF, $0000, $0320, $0060
-		dc.w $0004, $0000, $2FFF, $0000, $0320, $0060
+		lvlsize     0,  $500,  $110,  $110 ; good ending
+		lvlsize     0,  $DC0,  $110,  $110 ; bad ending
+		lvlsize     0, $2FFF,     0,  $320 ; (unused)
+		lvlsize     0, $2FFF,     0,  $320 ; (unused)
 		even
