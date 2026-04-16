c2ec5c4279ce12f32f3ad9d8489ad1c12e5a2148
Selbi182 <shadow93x@gmail.com>
2026-04-16 11:49:40 +0200

    Added more explanations to ASM68K build script

--- build.bat
+++ build.bat
@@ -1,11 +1,37 @@
-@echo off
+@ECHO OFF
 
-IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
-"build_tools\asm68k.exe" /k /m /p /o ae-,oz+,c+,l+ sonic.asm, s1built.bin >sonic.log, , sonic.lst
+REM // Rename previously successful build if one existed.
+IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin > NUL
+
+REM // Run the ASM68K assembly with the following parameters:
+REM //   k  >>  allow use of ifeq, etc.
+REM //   m  >>  expand macros in listing file
+REM //   p  >>  produce pure binary output file
+REM //   o ___  >>  set assembler options/optimisations:
+REM //     ae-  >>  disable automatic even on dc/dcb/ds/rs .w/l
+REM //     oz+  >>  enable zero offset optimisation
+REM //     c+   >>  enable case sensitivity
+REM //     l+   >>  use '.' as leading character for local labels
+REM // 
+REM // Files:
+REM //   sonic.asm    >>  input assembly file
+REM //   s1built.bin  >>  assembled ROM
+REM //   [blank]      >>  symbol file (disabled)
+REM //   sonic.lst    >>  listing file
+REM //   sonic.log    >>  console output redirected to log file
+"build_tools\asm68k.exe" /k /m /p /o ae-,oz+,c+,l+ sonic.asm, s1built.bin, , sonic.lst > sonic.log
+
+REM // Still print redirected log output to console (Batch doesn't suppport tee).
+type sonic.log
+
+REM // Fix checksum (only if output was generated).
 if exist s1built.bin (
-    "build_tools\fixheadr.exe" s1built.bin	
-) else (
-    type sonic.log
+    "build_tools\fixheadr.exe" s1built.bin
+)
+
+REM // If assembly produced warnings or errors, pause here so that the user can read them.
+findstr /i /c:"Warning :" /c:"Error :" sonic.log > NUL
+if not errorlevel 1 (
     echo.
-    pause REM // Pause on failure so that the user can read the error message.
-)	
+    pause
+)
