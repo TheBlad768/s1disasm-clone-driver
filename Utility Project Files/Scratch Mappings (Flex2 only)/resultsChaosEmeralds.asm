18c35b73da2e5687c07dd86355cb9c5bb530a4a0
Benjamin the Raccoon <bjclaridge1310@gmail.com>
2026-04-20 02:11:18 +1000

    Almost forgot the HUD and the SS Results Chaos Emeralds!

---
+++ Utility Project Files/Scratch Mappings (Flex2 only)/resultsChaosEmeralds.asm
@@ -0,0 +1,33 @@
+Map_4b09: mappingsTable
+	mappingsTableEntry.w	Map_4b09_0
+	mappingsTableEntry.w	Map_4b09_1
+	mappingsTableEntry.w	Map_4b09_2
+	mappingsTableEntry.w	Map_4b09_3
+	mappingsTableEntry.w	Map_4b09_4
+	mappingsTableEntry.w	Map_4b09_5
+
+Map_4b09_0:	spriteHeader
+ spritePiece -8, -8, 2, 2, 4, 0, 0, 1, 0
+Map_4b09_0_End
+
+Map_4b09_1:	spriteHeader
+ spritePiece -8, -8, 2, 2, 0, 0, 0, 0, 0
+Map_4b09_1_End
+
+Map_4b09_2:	spriteHeader
+ spritePiece -8, -8, 2, 2, 4, 0, 0, 2, 0
+Map_4b09_2_End
+
+Map_4b09_3:	spriteHeader
+ spritePiece -8, -8, 2, 2, 4, 0, 0, 3, 0
+Map_4b09_3_End
+
+Map_4b09_4:	spriteHeader
+ spritePiece -8, -8, 2, 2, 8, 0, 0, 1, 0
+Map_4b09_4_End
+
+Map_4b09_5:	spriteHeader
+ spritePiece -8, -8, 2, 2, $C, 0, 0, 1, 0
+Map_4b09_5_End
+
+	even
