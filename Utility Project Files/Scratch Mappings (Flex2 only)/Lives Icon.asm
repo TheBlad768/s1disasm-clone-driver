dc607c511a220a5780847479dd2ba7e73abcc131
Benjamin the Raccoon <bjclaridge1310@gmail.com>
2026-04-20 02:01:10 +1000

    Extended Flex2 Project (And scratch mappings. Yay.)

---
+++ Utility Project Files/Scratch Mappings (Flex2 only)/Lives Icon.asm
@@ -0,0 +1,9 @@
+Map_1ab5: mappingsTable
+	mappingsTableEntry.w	Map_1ab5_0
+
+Map_1ab5_0:	spriteHeader
+ spritePiece -$18, -8, 2, 2, 0, 0, 0, 0, 0
+ spritePiece -8, -8, 4, 2, 4, 0, 0, 0, 0
+Map_1ab5_0_End
+
+	even
---
+++ Utility Project Files/Scratch Mappings (Flex2 only)/TitleCardFont.asm
@@ -0,0 +1,161 @@
+Map_9d6b: mappingsTable
+	mappingsTableEntry.w	Map_9d6b_0
+	mappingsTableEntry.w	Map_9d6b_1
+	mappingsTableEntry.w	Map_9d6b_2
+	mappingsTableEntry.w	Map_9d6b_3
+	mappingsTableEntry.w	Map_9d6b_4
+	mappingsTableEntry.w	Map_9d6b_5
+	mappingsTableEntry.w	Map_9d6b_6
+	mappingsTableEntry.w	Map_9d6b_7
+	mappingsTableEntry.w	Map_9d6b_8
+	mappingsTableEntry.w	Map_9d6b_9
+	mappingsTableEntry.w	Map_9d6b_10
+	mappingsTableEntry.w	Map_9d6b_11
+	mappingsTableEntry.w	Map_9d6b_12
+	mappingsTableEntry.w	Map_9d6b_13
+	mappingsTableEntry.w	Map_9d6b_14
+	mappingsTableEntry.w	Map_9d6b_15
+	mappingsTableEntry.w	Map_9d6b_16
+	mappingsTableEntry.w	Map_9d6b_17
+	mappingsTableEntry.w	Map_9d6b_18
+	mappingsTableEntry.w	Map_9d6b_19
+	mappingsTableEntry.w	Map_9d6b_20
+	mappingsTableEntry.w	Map_9d6b_21
+	mappingsTableEntry.w	Map_9d6b_22
+	mappingsTableEntry.w	Map_9d6b_23
+	mappingsTableEntry.w	Map_9d6b_24
+	mappingsTableEntry.w	Map_9d6b_25
+	mappingsTableEntry.w	Map_9d6b_26
+	mappingsTableEntry.w	Map_9d6b_27
+	mappingsTableEntry.w	Map_9d6b_28
+
+Map_9d6b_0:	spriteHeader
+ spritePiece -8, -8, 2, 2, 0, 0, 0, 0, 0
+Map_9d6b_0_End
+
+Map_9d6b_1:	spriteHeader
+ spritePiece -8, -8, 2, 2, 4, 0, 0, 0, 0
+Map_9d6b_1_End
+
+Map_9d6b_2:	spriteHeader
+ spritePiece -8, -8, 2, 2, 8, 0, 0, 0, 0
+Map_9d6b_2_End
+
+Map_9d6b_3:	spriteHeader
+ spritePiece -8, -8, 2, 2, $C, 0, 0, 0, 0
+Map_9d6b_3_End
+
+Map_9d6b_4:	spriteHeader
+ spritePiece -8, -8, 2, 2, $10, 0, 0, 0, 0
+Map_9d6b_4_End
+
+Map_9d6b_5:	spriteHeader
+ spritePiece -8, -8, 2, 2, $14, 0, 0, 0, 0
+Map_9d6b_5_End
+
+Map_9d6b_6:	spriteHeader
+ spritePiece -8, -8, 2, 2, $18, 0, 0, 0, 0
+Map_9d6b_6_End
+
+Map_9d6b_7:	spriteHeader
+ spritePiece -8, -8, 2, 2, $1C, 0, 0, 0, 0
+Map_9d6b_7_End
+
+Map_9d6b_8:	spriteHeader
+ spritePiece -4, -8, 1, 2, $20, 0, 0, 0, 0
+Map_9d6b_8_End
+
+Map_9d6b_9:	spriteHeader
+ spritePiece -8, -8, 2, 2, $22, 0, 0, 0, 0
+Map_9d6b_9_End
+
+Map_9d6b_10:	spriteHeader
+ spritePiece -8, -8, 2, 2, $26, 0, 0, 0, 0
+Map_9d6b_10_End
+
+Map_9d6b_11:	spriteHeader
+ spritePiece -8, -8, 2, 2, $2A, 0, 0, 0, 0
+Map_9d6b_11_End
+
+Map_9d6b_12:	spriteHeader
+ spritePiece -8, -8, 2, 2, $2E, 0, 0, 0, 0
+Map_9d6b_12_End
+
+Map_9d6b_13:	spriteHeader
+ spritePiece -8, -8, 2, 2, $32, 0, 0, 0, 0
+Map_9d6b_13_End
+
+Map_9d6b_14:	spriteHeader
+ spritePiece -8, -8, 2, 2, $36, 0, 0, 0, 0
+Map_9d6b_14_End
+
+Map_9d6b_15:	spriteHeader
+ spritePiece -8, -8, 2, 2, $3A, 0, 0, 0, 0
+Map_9d6b_15_End
+
+Map_9d6b_16:	spriteHeader
+ spritePiece -8, -8, 2, 2, $3E, 0, 0, 0, 0
+Map_9d6b_16_End
+
+Map_9d6b_17:	spriteHeader
+ spritePiece -8, -8, 2, 2, $42, 0, 0, 0, 0
+Map_9d6b_17_End
+
+Map_9d6b_18:	spriteHeader
+ spritePiece -8, -8, 2, 2, $46, 0, 0, 0, 0
+Map_9d6b_18_End
+
+Map_9d6b_19:	spriteHeader
+ spritePiece -8, -8, 2, 2, $4A, 0, 0, 0, 0
+Map_9d6b_19_End
+
+Map_9d6b_20:	spriteHeader
+ spritePiece -8, -8, 2, 2, $4E, 0, 0, 0, 0
+Map_9d6b_20_End
+
+Map_9d6b_21:	spriteHeader
+ spritePiece -2, -6, 1, 1, $52, 0, 0, 0, 0
+Map_9d6b_21_End
+
+Map_9d6b_22:	spriteHeader
+ spritePiece -$C, -5, 3, 1, $53, 0, 0, 0, 0
+Map_9d6b_22_End
+
+Map_9d6b_23:	spriteHeader
+ spritePiece -4, -$18, 1, 4, $56, 0, 0, 0, 0
+Map_9d6b_23_End
+
+Map_9d6b_24:	spriteHeader
+ spritePiece -8, -$10, 2, 3, $5A, 0, 0, 0, 0
+Map_9d6b_24_End
+
+Map_9d6b_25:	spriteHeader
+ spritePiece -8, -$10, 2, 3, $60, 0, 0, 0, 0
+Map_9d6b_25_End
+
+Map_9d6b_26:	spriteHeader
+ spritePiece -$10, -7, 4, 2, $66, 0, 0, 0, 0
+Map_9d6b_26_End
+
+Map_9d6b_27:	spriteHeader
+ spritePiece -8, -9, 2, 1, $6E, 0, 0, 0, 0
+ spritePiece -8, -1, 2, 1, $6E, 1, 1, 0, 0
+Map_9d6b_27_End
+
+Map_9d6b_28:	spriteHeader
+ spritePiece -$C, -$1C, 4, 1, $70, 0, 0, 0, 0
+ spritePiece $14, -$1C, 1, 3, $74, 0, 0, 0, 0
+ spritePiece -$14, -$14, 2, 1, $77, 0, 0, 0, 0
+ spritePiece -$1C, -$C, 2, 2, $79, 0, 0, 0, 0
+ spritePiece -$14, $14, 4, 1, $70, 1, 1, 0, 0
+ spritePiece -$1C, 4, 1, 3, $74, 1, 1, 0, 0
+ spritePiece 4, $C, 2, 1, $77, 1, 1, 0, 0
+ spritePiece $C, -4, 2, 2, $79, 1, 1, 0, 0
+ spritePiece -4, -$14, 3, 1, $7D, 0, 0, 0, 0
+ spritePiece -$C, -$C, 4, 1, $7C, 0, 0, 0, 0
+ spritePiece -$C, -4, 3, 1, $7C, 0, 0, 0, 0
+ spritePiece -$14, 4, 4, 1, $7C, 0, 0, 0, 0
+ spritePiece -$14, $C, 3, 1, $7C, 0, 0, 0, 0
+Map_9d6b_28_End
+
+	even
