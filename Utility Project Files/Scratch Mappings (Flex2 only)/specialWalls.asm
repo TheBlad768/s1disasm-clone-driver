1363ddc62b88c902f5e7407df93f7b6ed5e23468
Benjamin the Raccoon <bjclaridge1310@gmail.com>
2026-04-21 03:23:05 +1000

    Fixed missing scratch mappings, and changed Sonic to S2 until the bug gets fixed.

---
+++ Utility Project Files/Scratch Mappings (Flex2 only)/specialWalls.asm
@@ -0,0 +1,88 @@
+Map_6487: mappingsTable
+	mappingsTableEntry.w	Map_6487_0
+	mappingsTableEntry.w	Map_6487_1
+	mappingsTableEntry.w	Map_6487_2
+	mappingsTableEntry.w	Map_6487_3
+	mappingsTableEntry.w	Map_6487_4
+	mappingsTableEntry.w	Map_6487_5
+	mappingsTableEntry.w	Map_6487_6
+	mappingsTableEntry.w	Map_6487_7
+	mappingsTableEntry.w	Map_6487_8
+	mappingsTableEntry.w	Map_6487_9
+	mappingsTableEntry.w	Map_6487_10
+	mappingsTableEntry.w	Map_6487_11
+	mappingsTableEntry.w	Map_6487_12
+	mappingsTableEntry.w	Map_6487_13
+	mappingsTableEntry.w	Map_6487_14
+	mappingsTableEntry.w	Map_6487_15
+	mappingsTableEntry.w	Map_6487_16
+
+Map_6487_0:	spriteHeader
+ spritePiece -$C, -$C, 3, 3, 0, 0, 0, 0, 0
+Map_6487_0_End
+
+Map_6487_1:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, 9, 0, 0, 0, 0
+Map_6487_1_End
+
+Map_6487_2:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $19, 0, 0, 0, 0
+Map_6487_2_End
+
+Map_6487_3:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $29, 0, 0, 0, 0
+Map_6487_3_End
+
+Map_6487_4:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $39, 0, 0, 0, 0
+Map_6487_4_End
+
+Map_6487_5:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $49, 0, 0, 0, 0
+Map_6487_5_End
+
+Map_6487_6:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $59, 0, 0, 0, 0
+Map_6487_6_End
+
+Map_6487_7:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $69, 0, 0, 0, 0
+Map_6487_7_End
+
+Map_6487_8:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $79, 0, 0, 0, 0
+Map_6487_8_End
+
+Map_6487_9:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $89, 0, 0, 0, 0
+Map_6487_9_End
+
+Map_6487_10:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $99, 0, 0, 0, 0
+Map_6487_10_End
+
+Map_6487_11:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $A9, 0, 0, 0, 0
+Map_6487_11_End
+
+Map_6487_12:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $B9, 0, 0, 0, 0
+Map_6487_12_End
+
+Map_6487_13:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $C9, 0, 0, 0, 0
+Map_6487_13_End
+
+Map_6487_14:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $D9, 0, 0, 0, 0
+Map_6487_14_End
+
+Map_6487_15:	spriteHeader
+ spritePiece -$10, -$10, 4, 4, $E9, 0, 0, 0, 0
+Map_6487_15_End
+
+Map_6487_16:	spriteHeader
+ spritePiece -$C, -$C, 3, 3, 0, 0, 0, 0, 0
+Map_6487_16_End
+
+	even
